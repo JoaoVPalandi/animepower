70925ae4224dded54911b03649c41689903bcbbb
Joao Palandi <j.palandi@aluno.senai.br>
2025-04-09 11:05:22 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/JoaoVPalandi/animepower/blob/main/script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JoaoVPalandi/animepower/refs/heads/main/script.lua"))()
